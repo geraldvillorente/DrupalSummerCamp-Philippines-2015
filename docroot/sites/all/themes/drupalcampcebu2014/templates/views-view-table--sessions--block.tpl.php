d98aad415ed71f3f6507556e2df29b8fd2014758
Mary Chris <marychris.cas@gmail.com>
2014-09-23 19:20:59 +0800

    Bug version for session tabs

---
+++ docroot/sites/all/themes/drupalcampcebu2014/templates/views-view-table--sessions--block.tpl.php
@@ -0,0 +1,69 @@
+<?php
+
+// This is a yemplate for session fade in and fade out. Day 1 and day 2.
+// Used a template to add id "day_one" and "day_two".
+
+/**
+ * @file
+ * Template to display a view as a table.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $header: An array of header labels keyed by field id.
+ * - $caption: The caption for this table. May be empty.
+ * - $header_classes: An array of header classes keyed by field id.
+ * - $fields: An array of CSS IDs to use for each field id.
+ * - $classes: A class or classes to apply to the table, based on settings.
+ * - $row_classes: An array of classes to apply to each row, indexed by row
+ *   number. This matches the index in $rows.
+ * - $rows: An array of row items. Each row is an array of content.
+ *   $rows are keyed by row number, fields within rows are keyed by field ID.
+ * - $field_classes: An array of classes to apply to each field, indexed by
+ *   field id, then row number. This matches the index in $rows.
+ * @ingroup views_templates
+ */
+
+$current_display = views_get_current_view();
+
+dpm($classes);
+
+$current_display_id = $current_display->current_display;
+
+// @TODO remove this from here and put it into template.php.
+if ($current_display_id == 'day_one') {
+  $active_class = ' active in';
+}
+else {
+  $active_class ='';
+}
+
+?>
+<div <?php print 'id="' . $current_display_id . '"'; ?> <?php print 'class="tab-pane fade' . $active_class . '"' ?>>
+<table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
+   <?php if (!empty($title) || !empty($caption)) : ?>
+     <caption><?php print $caption . $title; ?></caption>
+  <?php endif; ?>
+  <?php if (!empty($header)) : ?>
+    <thead>
+      <tr>
+        <?php foreach ($header as $field => $label): ?>
+          <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
+            <?php print $label; ?>
+          </th>
+        <?php endforeach; ?>
+      </tr>
+    </thead>
+  <?php endif; ?>
+  <tbody>
+    <?php foreach ($rows as $row_count => $row): ?>
+      <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
+        <?php foreach ($row as $field => $content): ?>
+          <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+            <?php print $content; ?>
+          </td>
+        <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+</div>
+</div>
